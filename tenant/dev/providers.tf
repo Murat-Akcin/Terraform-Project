06766d09a241f543c1da4b258d1148878a8362d4
Murat <akcin.murat@hotmail.com>
2025-07-03 23:06:52 +0300

    first commit

---
+++ tenant/dev/providers.tf
@@ -0,0 +1,22 @@
+##### Define Terraform State Backend and Must Providers
+terraform {
+  backend "s3" {
+    bucket  = "tf-remote-backend-murat"
+    key     = "proje/dev/infrastructure.tfstate"
+    profile = "default"
+    region  = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+}
+
+##### Give Provider Credentials
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
